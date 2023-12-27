b48a43b393e576ca473f2c923297ecd3f507e9a1
mahak.garg <gargmahak1998@gmail.com>
2023-12-28 00:29:21 +0530

    user registration

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,7 @@
 const Post = require('../models/PostModel');
 const Category = require('../models/CategoryModel');
+const bcrypt = require('bcrypt');
+const User = require('../models/UserModel');
 
 module.exports = {
 
@@ -21,6 +23,49 @@ module.exports = {
     },
     
     registerPost: (req, res ) => {
-        res.send("Registered Successfully");
+       // res.send("Registered Successfully");
+       let errors = [];
+
+       if (!req.body.firstName){
+        errors.push({message: 'First name is mandatory'});
+       }
+       if (!req.body.lastName){
+        errors.push({message: 'Last name is mandatory'});
+       }
+       if (!req.body.email){
+        errors.push({message: 'Email field is mandatory'});
+       }
+       if (req.body.password != req.body.passwordConfirm){
+        errors.push({message: ' Password do not match'});
+       }
+
+       if(errors.length > 0){
+        res.render('default/register',{
+         errors : errors,
+         firstName: req.body.firstName,
+         lastName: req.body.lastName,
+         email: req.body.email
+});
+       }
+       else {
+        User.findOne({ email: req.body.email}).then( user =>{
+            if(user){
+                req.flash('error-message','Email already exists, try to login');
+                res.redirect('/login');
+            }
+            else{
+                const newUser = new User(req.body);
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        newUser.password = hash;
+                        newUser.save().then(user => {
+                            req.flash('success-message', 'You are now registered');
+                            res.redirect('/login')
+                        });
+                    });
+                });
+            }
+        })
+       }
     }
 };
